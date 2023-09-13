bb762414dfd6ebd33a425552ba4ba00c7562a481
zara <nz.rasekh@gmail.com>
2023-09-13 23:36:52 +0330

    added evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+num=input('please enter a num: ')
+
+def myfunc(num):
+    num=int(num)
+    
+    if num%2==0:
+        print('its even')
+    else:
+        print('its odd')
+
+myfunc(num)        
